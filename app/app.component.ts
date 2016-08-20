0cccd80f2e8cd73c72b80913d092c191efba833f
Puneeth Kumar Reddy Melavoy Raja <puneethmr1@gmail.com>
2016-08-20 12:05:11 -0400

    Added Services component and made it injectable into App Component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Hero} from './hero';
-
+import {HeroService} from './hero.service';
+import {OnInit} from '@angular/core';
 
 @Component({
     selector:'my-app',
@@ -8,7 +9,7 @@ import {Hero} from './hero';
     <my-hero-detail [hero]="selectedHero" ></my-hero-detail>
     <div>
         <ul class="heroes">
-            <li *ngFor="let hero of heros"
+            <li *ngFor="let hero of heroes"
                 [class.selected]="hero === selectedHero"
                 (click)="onSelect(hero)">
                 <span class="badge">{{hero.id}}</span>
@@ -66,33 +67,29 @@ import {Hero} from './hero';
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+providers:[HeroService]
 
 })
 
-export class AppComponent{
-    heros = HEROES;
-    title = 'Tour of Heros';
-    // hero:Hero = {
-    //     id:1,
-    //     name:'Windstorm'
-    // };
+export class AppComponent implements OnInit{
+
+    constructor(private heroService:HeroService){ }
+
+    ngOnInit():void{
+      this.getHeroes();
+    }
+
+    heroes:Hero[];
+    title = 'Tour of Heroes';
     selectedHero:Hero;
 
+    getHeroes():void{
+      // this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+      this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
     onSelect(hero:Hero){
         this.selectedHero = hero;
     }
 }
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
